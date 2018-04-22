e978040c56848734e98c906c27c31b6d07aab226
Vinicius Castro <vinicius1595@gmail.com>
2018-04-22 19:27:31 -0300

    consulting route of monitoring

--- modules/monitoring/monitoringDAO.js
+++ modules/monitoring/monitoringDAO.js
@@ -15,5 +15,37 @@ module.exports.getMonitoring = async (callback) => {
     } catch (e) {
         return e.message;
     }
+}
 
+module.exports.getMonitoringByCurso = async (callback, course) => {
+    try {
+        let con = mySql.Connection();
+        con.connect(async (err) => {
+            if (err) {
+                console.error('error connecting: ' + err.message);
+                return err.message;
+            }
+            await con.query('select * from monitoria as m inner join curso as c on m.idCurso=?', course, callback);
+            con.end();
+        });
+    } catch (e) {
+        return e.message;
+    }
+}
+
+module.exports.getByCursoAndMonitoring = async (callback, course, monitoring) => {
+    let nameMonitoring = '%' + monitoring + '%';
+    try {
+        let con = mySql.Connection();
+        con.connect(async (err) => {
+            if (err) {
+                console.error('error connecting: ' + err.message);
+                return err.message;
+            }
+            await con.query('select * from monitoria as m inner join curso as c on m.idCurso=? and m.nome like ?', [course, nameMonitoring], callback);
+            con.end();
+        });
+    } catch (e) {
+        return e.message;
+    }
 }
--- modules/monitoring/monitoringRoute.js
+++ modules/monitoring/monitoringRoute.js
@@ -3,12 +3,36 @@ const router = express.Router();
 const monitoringDAO = require('./monitoringDAO');
 
 router.get('/monitoring', async (req, res) => {
-
   try {
     monitoringDAO.getMonitoring(function (error, result, fields) {
       if (error) return error.message;
       res.status(200).json(result);
     });
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
+});
+
+router.get('/monitoring/:course', async (req, res) => {
+  try {
+    let course = req.params.course;
+    monitoringDAO.getMonitoringByCurso(function (error, result, fields) {
+      if (error) return error.message;
+      res.status(200).json(result);
+    }, course);
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
+});
+
+router.get('/monitoring/:course/:monitoring', async (req, res) => {
+  try {
+    let course = req.params.course;
+    let monitoring = req.params.monitoring;
+    monitoringDAO.getByCursoAndMonitoring(function (error, result, fields) {
+      if (error) return error.message;
+      res.status(200).json(result);
+    }, course, monitoring);
   } catch (e) {
     res.status(400).json(e.message);
   }
