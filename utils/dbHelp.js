b1c620529771b9186ca3295fde06a34153791e4b
Vinicius Castro <vinicius1595@gmail.com>
2018-05-06 09:56:58 -0300

    Configs connection database

--- utils/dbHelp.js
+++ utils/dbHelp.js
@@ -4,11 +4,11 @@ var mysql = require('mysql');
 module.exports.Connection = () => {
 
 	var conMySql = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '1234',
-		database: 'unamonitoria'
-		//port: 3306
+		host: process.env.serverMonitor,
+		user: process.env.userMonitor,
+		password: process.env.passMonitor,
+		database: 'unamonitoria',
+		port: 41890
 	});
 	
 	return conMySql;
