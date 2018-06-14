973e5c2fb4333dd185630254100fdbb1e5e6e124
Vinicius Castro <vinicius1595@gmail.com>
2018-06-13 23:01:44 -0300

    bd connection

---
+++ utils/dbHelp.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+
+module.exports.Connection = () => {
+
+	var conMySql = mysql.createConnection({
+		host: process.env.serverMonitor,
+		user: process.env.userMonitor,
+		password: process.env.passMonitor,
+		database: 'unamonitoria',
+		port: 41890
+	});
+	
+	return conMySql;
+}
