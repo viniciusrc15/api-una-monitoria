64ae8c1e0592210c05f56b9dfe158b09767439d5
Vinicius Castro <vinicius1595@gmail.com>
2018-06-24 19:57:04 -0300

    id_monitoria in query select

--- modules/monitoring/monitoringDAO.js
+++ modules/monitoring/monitoringDAO.js
@@ -40,7 +40,7 @@ module.exports.getMonitoringByCurso = async (callback, course) => {
                 console.error('error connecting: ' + err.message);
                 return err.message;
             }
-            await con.query('SELECT mo.nome as monitor, dis.nome as disciplina, cu.nome as curso, mt.horario, mt.localizacao FROM  monitor AS mo inner join monitoria  as mt on mo.id_monitor = mt.monitor_id_monitor inner join disciplina as dis on mt.disciplina_id_disciplina = dis.id_disciplina inner join curso_has_disciplina as ch on dis.id_disciplina = ch.disciplina_id_disciplina inner join curso as cu on ch.curso_id_curso = cu.id_curso WHERE cu.id_curso = ? and ativo=1', course, callback);
+            await con.query('SELECT mt.id_monitoria, mo.nome as monitor, dis.nome as disciplina, cu.nome as curso, mt.horario, mt.localizacao FROM  monitor AS mo inner join monitoria  as mt on mo.id_monitor = mt.monitor_id_monitor inner join disciplina as dis on mt.disciplina_id_disciplina = dis.id_disciplina inner join curso_has_disciplina as ch on dis.id_disciplina = ch.disciplina_id_disciplina inner join curso as cu on ch.curso_id_curso = cu.id_curso WHERE cu.id_curso = ? and ativo=1', course, callback);
             con.end();
         });
     } catch (e) {
@@ -57,7 +57,7 @@ module.exports.getByCursoAndMonitoring = async (callback, course, monitoring) =>
                 console.error('error connecting: ' + err.message);
                 return err.message;
             }
-            await con.query('SELECT mo.nome as monitor, dis.nome as disciplina, cu.nome as curso, mt.horario, mt.localizacao FROM  monitor AS mo inner join monitoria  as mt on mo.id_monitor = mt.monitor_id_monitor inner join disciplina as dis on mt.disciplina_id_disciplina = dis.id_disciplina inner join curso_has_disciplina as ch on dis.id_disciplina = ch.disciplina_id_disciplina inner join curso as cu on ch.curso_id_curso = cu.id_curso WHERE cu.id_curso = ? and dis.nome like ? and ativo=1', [course, nameMonitoring], callback);
+            await con.query('SELECT mt.id_monitoria, mo.nome as monitor, dis.nome as disciplina, cu.nome as curso, mt.horario, mt.localizacao FROM  monitor AS mo inner join monitoria  as mt on mo.id_monitor = mt.monitor_id_monitor inner join disciplina as dis on mt.disciplina_id_disciplina = dis.id_disciplina inner join curso_has_disciplina as ch on dis.id_disciplina = ch.disciplina_id_disciplina inner join curso as cu on ch.curso_id_curso = cu.id_curso WHERE cu.id_curso = ? and dis.nome like ? and ativo=1', [course, nameMonitoring], callback);
             con.end();
         });
     } catch (e) {
